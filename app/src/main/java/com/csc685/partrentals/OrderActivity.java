0dafc929023e6439adc432fa54210fc13f2c7837
gitobu <allangitobu@hotmail.com>
2025-07-16 16:35:26 -0700

    Version 1.0 Initial Code

---
+++ app/src/main/java/com/csc685/partrentals/OrderActivity.java
@@ -0,0 +1,107 @@
+package com.csc685.partrentals;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.ToggleButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_order);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        buttonOrderAction();
+        orderSummaryAction();
+        initToggleButton();
+        saveAction();
+
+    }
+    private void initToggleButton() {
+        final ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButtonEditSave);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setForEditing(toggleButton.isChecked());
+                setForEditing(true);
+            }
+        });
+    }
+    private void setForEditing(boolean enabled) {
+        EditText editMake = findViewById(R.id.editTextCustomerName);
+        editMake.setEnabled(enabled);
+        EditText editModel = findViewById(R.id.editTextPhoneNumber);
+        editModel.setEnabled(enabled);
+        EditText editCategory = findViewById(R.id.editTextAddress);
+        editCategory.setEnabled(enabled);
+        EditText editCity = findViewById(R.id.editTextCity);
+        editCity.setEnabled(enabled);
+        EditText editState = findViewById(R.id.editTextState);
+        editState.setEnabled(enabled);
+        EditText editZipCode = findViewById(R.id.editTextZipCode);
+        editZipCode.setEnabled(enabled);
+        EditText editItem = findViewById(R.id.editTextRentalItem);
+        editItem.setEnabled(enabled);
+        EditText editDescription = findViewById(R.id.editTextItemDescription);
+        editDescription.setEnabled(enabled);
+        EditText editQuantity = findViewById(R.id.editTextQuantity);
+        editQuantity.setEnabled(enabled);
+        if (enabled) {
+            editMake.requestFocus();
+        } else {
+            ScrollView s = findViewById(R.id.scrollViewControls);
+            s.fullScroll(ScrollView.FOCUS_UP);
+        }
+    }
+    private void saveAction(){
+        Button button = findViewById(R.id.buttonSave);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OrderActivity.this, OrderSummaryActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void buttonOrderAction(){
+        ImageButton address = findViewById(R.id.imageButtonOrder);
+        address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OrderActivity.this, OrderActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void orderSummaryAction(){
+        ImageButton orders = findViewById(R.id.imageButtonSummary);
+        orders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OrderActivity.this, OrderSummaryActivity.class);
+                String referenced_activity = "You are on Orders Activity";
+                intent.putExtra("destination_activity", referenced_activity);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+}
